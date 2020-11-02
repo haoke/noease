6035864ffa9d958ecd05a72201cf65e19628dead
haoke <haoke.wu@gmail.com>
2020-11-02 18:18:15 +0800

    Create Upload.java

---
+++ java/spring_study/springMVC/upload/Upload.java
@@ -0,0 +1,31 @@
+@Controller
+public class FileUpload {
+
+    @RequestMapping(path="/upload.do",method = RequestMethod.POST)
+    public String upload(MultipartFile mfile, HttpServletRequest req) throws IOException {
+
+        String  path = req.getServletContext().getRealPath("/uploadDir");
+        System.out.println(path);
+        File file = new File(path);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+
+        String originalFilename = mfile.getOriginalFilename();
+        mfile.transferTo(new File(path, originalFilename));
+        return "success";
+    }
+
+
+    public String remoteUpdate(MultipartFile mfile) throws IOException {
+        String path = "http://localhost:9090/fileserver/uploads";
+        String originalFilename = mfile.getOriginalFilename();
+
+        Client client= new Client();
+        WebResource resource = client.resource(path+ originalFilename);
+        resource.put(mfile.getBytes());
+
+        return "success";
+
+    }
+}
